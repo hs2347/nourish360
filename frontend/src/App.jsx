56308ba7c323fc0c3594eaff14ff87fca2401e15
basundhara-070 <2022ugec070@nitjsr.ac.in>
2024-05-29 12:31:11 +0530

    Added Auth0

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,15 +4,24 @@ import Navbar from './components/Home/Navbar';
 import Join from './components/Home/Join';
 import AboutUs from './components/Home/AboutUs';
 import Vision from './components/Home/Vision';
+import { createRoot } from 'react-dom/client';
+import { Auth0Provider } from '@auth0/auth0-react';
 function App() {
 
    return (
+    <Auth0Provider
+    domain="dev-wx0wzhodkgnv7da8.us.auth0.com"
+    clientId="ZKi2jjNqTOLitVw0zK1hEZ8FtO4soPty"
+    authorizationParams={{
+      redirect_uri: window.location.origin
+    }}>
     <>
      <Navbar/>
      <Join/>
      <AboutUs/>
      <Vision/>
     </>
+    </Auth0Provider>
   );
 }
 
--- frontend/src/components/Home/Navbar.jsx
+++ frontend/src/components/Home/Navbar.jsx
@@ -2,20 +2,38 @@
 import React from 'react';
 import './Navbar.css';
 import logo from '../../assets/logo.png'; // Import your logo image
+import { useAuth0 } from "@auth0/auth0-react";
 
 const Navbar = () => {
+  const { user, isAuthenticated, loginWithRedirect,logout } = useAuth0();
+  console.log(user);
   return (
     <nav>
       <div className="left">
         <img src={logo} alt="Logo" className="logo" />
       </div>
       <div className="right">
-        <div id="nav-part2">
+        {
+        !isAuthenticated&&(
+          <div id="nav-part2">
           <h4><a href="#home">Home</a></h4>
           <h4><a href="#blog">Blog</a></h4>
-          <h4><a href="#services">Login</a></h4>
-          <h4><a href="#register">Register</a></h4>
+          <h4 onClick={() => loginWithRedirect()}><a href="#login">Login</a></h4>
+          <h4 onClick={() => loginWithRedirect()}><a href="#signup">Sign up</a></h4>
         </div>
+        )
+        }
+         {
+        isAuthenticated&&(
+          <div id="nav-part2">
+          <h4><a href="#home">Home</a></h4>
+          <h4><a href="#blog">Blog</a></h4>
+          <h4 onClick={() => logout({ logoutParams: { returnTo: window.location.origin } })}><a href="#logout">Logout</a></h4>
+          <><a href="#profile"><img src={user.picture} className='rounded-[50%] aspect-square w-16' alt="user profile" /></a></>
+        </div>
+        )
+        }
+        
       </div>
     </nav>
   );
