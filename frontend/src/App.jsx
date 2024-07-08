02343ee4f4b2911ef8d8ef315f0cd0167c7e3b05
hs2347 <142593123+hs2347@users.noreply.github.com>
2024-07-08 07:55:36 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,16 +1,21 @@
 import React from 'react';
-<<<<<<< HEAD
+
  import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-=======
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
->>>>>>> 0f9990975651182f6f6d194ba8f942e22560b550
+
 import Navbar from './components/Home/Navbar';
 import Join from './components/Home/Join';
 import AboutUs from './components/Home/AboutUs';
 import Vision from './components/Home/Vision';
-<<<<<<< HEAD
+
 import Donate from './components/Donate/Donate';
 import DonateButton from './components/Donate/DonateButton';
+import Events from './components/Events';
+import AddEvent from './components/AddEvent'; 
+import { Auth0Provider } from '@auth0/auth0-react';
+import Organisations from './components/Organisations';
+import AddOrganisation from './components/AddOrganisation';
+import Hero from './components/Hero';
+import MapComponent from './components/Map';
 //import PaymentSuccess from "./PaymentSuccess";
 function Home() {
   return (
@@ -24,27 +29,8 @@ function Home() {
   );
 }
 
-function App() {
-  return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/donate" element={<Donate />} />
-        
-      </Routes>
-    </Router>
-  );
-}
 
-export default App
-=======
-import Events from './components/Events';
-import AddEvent from './components/AddEvent'; 
-import { Auth0Provider } from '@auth0/auth0-react';
-import Organisations from './components/Organisations';
-import AddOrganisation from './components/AddOrganisation';
-import Hero from './components/Hero';
-import MapComponent from './components/Map';
+
 
 function App() {
   return (
@@ -58,6 +44,7 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
+           <Route path="/donate" element={<Donate />} />
           <Route path="/" element={<><Hero/><Join /><AboutUs /><Vision /><Events /><Organisations /></>} />
           <Route path="/add-event" element={<AddEvent />} />
           <Route path="/add-organisation" element={<AddOrganisation />} />
@@ -69,4 +56,4 @@ function App() {
 }
 
 export default App;
->>>>>>> 0f9990975651182f6f6d194ba8f942e22560b550
+
